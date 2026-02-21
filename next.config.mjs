4780e9dbdbbf2b871912ed75cf3788624c1c18bd
Ian Lopes <160206746+IAN08527@users.noreply.github.com>
2026-02-21 10:50:36 +0530

    fixing hugginface dependency in vercel error with chat feature

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  serverComponentsExternalPackages: ["@huggingface/transformers"],
 };
 
 export default nextConfig;
